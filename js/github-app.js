765e13db8346ce90fe7af2abd152a7a59e999ad6
Tin Ho <tintho@uw.edu>
2014-11-06 12:16:43 -0800

    no message

--- js/github-app.js
+++ js/github-app.js
@@ -8,11 +8,21 @@
 
 
 angular.module('GitHubApp', [])
-    .controller('GitHubController', function($scope) {
+    .controller('GitHubController', function($scope, $http) {
         $scope.userName = 'drstearns';
         $scope.getRepos = function() {
 
-            //add code here
-
+            $scope.loading = true;
+            $http.get('https://api.github.com/users/' + $scope.userName + '/repos')
+                .success(function(data) {
+                    $scope.repos = data;
+                    $scope.errorMessage = null;
+                })
+                .error(function(err) {
+                    $scope.errorMessage  = err.message;
+                })
+                .finally(function() {
+                    $scope.loading = false;
+                })
         };
     });
